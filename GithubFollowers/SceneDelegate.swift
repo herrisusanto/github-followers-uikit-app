1e99f6d96d9d5ef19c700bf2dfc78b43e90167a5
Heri Susanto <herisusanto.app@gmail.com>
2024-02-15 16:31:59 +0700

    Add assets Add Navigation controllers Add Favorites & Search view controller

--- GithubFollowers/SceneDelegate.swift
+++ GithubFollowers/SceneDelegate.swift
@@ -17,8 +17,34 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = createTabBar()
         window?.makeKeyAndVisible()
+    }
+    
+    func createSearchNavigationController() -> UINavigationController {
+        let searchViewController = SearchViewController()
+        searchViewController.title = "Search"
+        searchViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchViewController)
+    }
+    
+    func createFavoritesNavigationController() -> UINavigationController {
+        let favoritesViewController = FavoritesListViewController()
+        favoritesViewController.title = "Favorites"
+        favoritesViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesViewController)
+    }
+    
+    func createTabBar() -> UITabBarController {
+        let tabBar = UITabBarController()
+        UITabBar.appearance().tintColor = .systemGreen 
+        tabBar.viewControllers = [
+            createSearchNavigationController(),
+            createFavoritesNavigationController()
+        ]
+        return tabBar
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ GithubFollowers/ViewControllers/FavoritesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesViewController.swift
+//  GithubFollowers
+//
+//  Created by loratech on 15/02/24.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad() 
+        view.backgroundColor = .white
+    }
+
+}
