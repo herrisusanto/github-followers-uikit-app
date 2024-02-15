a88a8a83ec703a721d1623269fb8939edc8e6833
Heri Susanto <herisusanto.app@gmail.com>
2024-02-15 14:16:17 +0700

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by loratech on 15/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
