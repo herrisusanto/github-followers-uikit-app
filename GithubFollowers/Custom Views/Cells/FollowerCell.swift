3a8174805d717298b12b519adb60a3b3dc7f8f09
Heri Susanto <herisusanto.app@gmail.com>
2024-02-19 13:18:36 +0700

    Add Follower Cell Add UIHelper Add GF Avatar Image View

---
+++ GithubFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,12 @@
+//
+//  FollowerCell.swift
+//  GithubFollowers
+//
+//  Created by loratech on 18/02/24.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+}
---
+++ GithubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,20 @@
+//
+//  GFAvatarImageView.swift
+//  GithubFollowers
+//
+//  Created by loratech on 18/02/24.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
