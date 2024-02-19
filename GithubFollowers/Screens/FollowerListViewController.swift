51ef67a6902c5f617ab52ba123cbdf1be3887fea
Heri Susanto <herisusanto.app@gmail.com>
2024-02-19 20:29:49 +0700

    Handle search username

--- GithubFollowers/Screens/FollowerListViewController.swift
+++ GithubFollowers/Screens/FollowerListViewController.swift
@@ -8,11 +8,13 @@
 import UIKit
 
 class FollowerListViewController: UIViewController {
+    
     enum Section {
         case main
     }
-    var username: String!
+    var username: String! = "herisusantuy"
     var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
     var page: Int = 1
     var hasMoreFollowers: Bool = true
     
@@ -23,6 +25,7 @@ class FollowerListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureCollectionView()
         getFollowers(username: username, page: page)
         configureDataSource()
@@ -48,6 +51,15 @@ class FollowerListViewController: UIViewController {
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
+    func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Search for a username..."
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+    }
+    
     
     
     func getFollowers(username: String, page: Int) {
@@ -70,7 +82,7 @@ class FollowerListViewController: UIViewController {
                         self.showEmptyStateView(with: message, in: self.view)
                     }
                 }
-                self.updateData()
+                self.updateData(on: followers)
                 
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
@@ -86,7 +98,7 @@ class FollowerListViewController: UIViewController {
         })
     }
     
-    func updateData() {
+    func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -106,6 +118,20 @@ extension FollowerListViewController: UICollectionViewDelegate {
             guard hasMoreFollowers else { return}
             getFollowers(username: username, page: page + 1)
         }
+    }
+}
+
+extension FollowerListViewController: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else {
+            return
+        }
+        filteredFollowers = followers.filter{ $0.login.lowercased().contains(filter.lowercased())}
+        updateData(on: filteredFollowers)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        updateData(on: followers)
     }
 }
 
