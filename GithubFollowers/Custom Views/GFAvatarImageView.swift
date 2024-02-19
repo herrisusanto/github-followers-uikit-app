ea817bf015a7580f17ed6be03733f704dfc9b983
Heri Susanto <herisusanto.app@gmail.com>
2024-02-19 13:42:53 +0700

    Handle cache image

--- GithubFollowers/Custom Views/GFAvatarImageView.swift
+++ GithubFollowers/Custom Views/GFAvatarImageView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
     
+    let cache = NetworkManager.shared.cache
     let placeholderImage = UIImage(named: "avatar-placeholder-dark")
 
     override init(frame: CGRect) {
@@ -24,8 +25,44 @@ class GFAvatarImageView: UIImageView {
         layer.cornerRadius = 10
         clipsToBounds = true
         image = placeholderImage
-        translatesAutoresizingMaskIntoConstraints = false 
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func downloadImage(from urlString: String) {
+        // MARK:  Checking if there's a cache for the image so no need to download it when it's available in cache
+        let cacheKey = NSString(string: urlString)
         
+        if let image = cache.object(forKey: cacheKey){
+            self.image = image
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            
+            guard let self = self else { return }
+            
+            if error != nil {
+                return
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                return
+            }
+            guard let data else { return }
+            
+            guard let image = UIImage(data: data) else {
+                return
+            }
+            // MARK:  Saving downloaded image to cache
+            self.cache.setObject(image, forKey: cacheKey)
+            
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+        task.resume()
     }
     
 }
